66dc58532c047972c0938f6633275d6439f2196b
YuriSschinelli <62344988+YuriSschinelli@users.noreply.github.com>
2024-11-05 22:31:25 -0300

    Update Content.js

--- Site_Blocker/Content.js
+++ Site_Blocker/Content.js
@@ -5,8 +5,11 @@ const generateHTML = (pageName) => { //cria a pagina HTML que substituirá o sit
 switch (window.location.hostname) { //varre a lista de sites a serem bloqueados pelo usuário e os bloqueia
     case "www.facebook.com":
         document.body.innerHTML = generateHTML("facebook")
+        break;
     case "www.netflix.com":
         document.body.innerHTML = generateHTML("Netflix")
+        break;
     case "www.instagram.com":
         document.body.innerHTML = generateHTML("Instagram")
+        break;
 }
